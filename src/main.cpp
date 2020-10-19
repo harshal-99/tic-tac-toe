923f4a315cb60caf02000fff55663aab4b9ac98c
harshal-99 <harshalpawshekar999@gmail.com>
2020-10-19 22:20:06 +0530

    Circle drawn at correct place and a little more file tuning is needed for drawing X.

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,15 @@ int main() {
 
 			std::array<sf::RectangleShape, 4> rect;
 			sf::RectangleShape outer;
-			std::array<char, 9> board{'-'};
+			std::array<int, 9> board{0};
+			sf::CircleShape circle;
+			sf::RectangleShape x_left,x_right;
+
+			circle.setPosition(outer.getPosition().x + 35, outer.getPosition().y + 35);
+			circle.setRadius(50.F);
+			circle.setOutlineThickness(2);
+			circle.setOutlineColor(sf::Color::Black);
+			circle.setPointCount(400);
 
 			outer.setPosition(3.F,3.F);
 			outer.setSize(sf::Vector2f(480.F, 480.F ));
@@ -40,14 +48,25 @@ int main() {
 			rect[3].setFillColor(sf::Color::Black);
 			rect[3].setSize(sf::Vector2f(480.F, 2.F));
 
+			x_left.setPosition(sf::Vector2f(185.F,35.F));
+			x_left.setFillColor(sf::Color::Black);
+			x_left.setSize(sf::Vector2f(135.F,3.F));
+			x_left.rotate(45.F);
 
+			x_right.setPosition(275.F,35.F);
+			x_right.setFillColor(sf::Color::Black);
+			x_right.setSize(sf::Vector2f(135.F, 3.F));
+			x_right.rotate(135.F);
 
 			window.clear(sf::Color::White);
 
 			window.draw(outer);
+			window.draw(circle);
 			for(const sf::RectangleShape& r : rect) {
 				window.draw(r);
 			}
+			window.draw(x_left);
+			window.draw(x_right);
 
 			window.display();
 		}
